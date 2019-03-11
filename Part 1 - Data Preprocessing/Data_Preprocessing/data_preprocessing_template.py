15b1cf2d46ccb0c14eddedb19e5dd1e28f0a1e93
Barbosa Stern <lgbarbosa@indracompany.com>
2019-03-11 20:31:30 -0300

    First

---
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,35 @@
+# Data Preprocessing
+
+# Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing dataset
+dataset = pd.read_csv('Data.csv')
+# Alocate all the lines, and all the columns except the last one to a matrix
+x = dataset.iloc[:, :-1].values
+# Alocate all the lines, and only the last column to a matrix
+y = dataset.iloc[:, -1].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+# Define strategy
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
+# Read X and then use the strategy to update X
+x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
+
+
+
+# Encoding categorial data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+# For X:
+labelencoder_x = LabelEncoder()
+# Update the collumn 0 of X with the encoding
+x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
+onehotencoder = OneHotEncoder(categorical_features=[0])
+x = onehotencoder.fit_transform(x).toarray()
+
+# For Y:
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
