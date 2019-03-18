294d26cb82d0b9eb277b5688744cbd4e6466d598
Lucas Stern <lucas.stern@hotmail.com.br>
2019-03-18 00:03:44 -0300

    Adicionado Mindmapping sobre Data Preprocessing e feito no script o ultimo ponto: Feature Scalling

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -37,3 +37,10 @@ y = labelencoder_y.fit_transform(y)
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+# Feature Scalling
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_tranform(x_train)
+x_test = sc_x.tranform(x_test)
