60aef5334679b44a599a4a72a73fd561b7fe071c
Lucas Stern <lucas.stern@hotmail.com.br>
2019-03-18 00:10:35 -0300

    Melhorias no Mindmapping de Data Preprocessing e definição do tempalte de data preprocessing

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -7,40 +7,17 @@ import pandas as pd
 
 # Importing dataset
 dataset = pd.read_csv('Data.csv')
-# Alocate all the lines, and all the columns except the last one to a matrix
 x = dataset.iloc[:, :-1].values
-# Alocate all the lines, and only the last column to a matrix
 y = dataset.iloc[:, -1].values
-
-# Taking care of missing data
-from sklearn.preprocessing import Imputer
-# Define strategy
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-# Read X and then use the strategy to update X
-x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
-
-
-
-# Encoding categorial data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-# For X:
-labelencoder_x = LabelEncoder()
-# Update the collumn 0 of X with the encoding
-x[:, 0] = labelencoder_x.fit_transform(x[:, 0])
-onehotencoder = OneHotEncoder(categorical_features=[0])
-x = onehotencoder.fit_transform(x).toarray()
-
-# For Y:
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
 
-
 # Feature Scalling
+"""
 from sklearn.preprocessing import StandardScaler
 sc_x = StandardScaler()
-x_train = sc_x.fit_tranform(x_train)
-x_test = sc_x.tranform(x_test)
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
+"""
