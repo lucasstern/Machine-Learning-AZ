d06431b94a9e51bf6069ba4251cb25f123bf5802
Lucas Stern <lucas.stern@hotmail.com.br>
2019-03-19 23:29:51 -0300

    MindMap: Regression - Simple Linear Regression

--- Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/Data_Preprocessing/data_preprocessing_template.py
@@ -7,17 +7,17 @@ import pandas as pd
 
 # Importing dataset
 dataset = pd.read_csv('Data.csv')
-x = dataset.iloc[:, :-1].values
+X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scalling
 """
 from sklearn.preprocessing import StandardScaler
-sc_x = StandardScaler()
-x_train = sc_x.fit_transform(x_train)
-x_test = sc_x.transform(x_test)
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
 """
