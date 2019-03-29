65b1be8b77bb482bcf3cdfe9264ea5650120d37b
luguistern <lu.guistern@gmail.com>
2019-03-28 23:04:40 -0300

    Section 5: Fitted a Multiple Linear Regression, with the "garbage"

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -11,9 +11,10 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 4].values
 
 # Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
@@ -21,7 +22,7 @@ X = onehotencoder.fit_transform(X).toarray()
 X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
@@ -32,10 +33,10 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Multiple Linear Regression to the Training set
+# Fitting Multiple Linear Regression to the Training Set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
+# Predicting the Test set Results
 y_pred = regressor.predict(X_test)
