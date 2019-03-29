8236559dc384dfea0b524b3a6d94e0a8bbe60a1d
luguistern <lu.guistern@gmail.com>
2019-03-29 00:19:49 -0300

    Section 5: Removing the garbage using backward elimination

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -40,3 +40,53 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set Results
 y_pred = regressor.predict(X_test)
+
+# Building the optimal model using backward elimination
+import statsmodels.formula.api as sm
+# Making a column with just ones, so the statsmodel librarie undestand the b0,
+# on the multiple linear regression formula
+X = np.append(arr=np.ones((50, 1)).astype(int), values=X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+# Step 1 - Define Significance Level
+SL = 0.05
+# Step 2 - Fit
+regressorOLS = sm.OLS(endog=y, exog=X_opt).fit()
+# Step 3 - Get the predictor with the highest P-Value
+regressorOLS.summary()
+# Step 4 - Remove the IV found on step 3 and repeat step 2 until
+# theres no more P < SL
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressorOLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressorOLS.summary()
+X_opt = X[:, [0, 3, 4, 5]]
+regressorOLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressorOLS.summary()
+X_opt = X[:, [0, 3, 5]]
+regressorOLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressorOLS.summary()
+X_opt = X[:, [0, 3]]
+regressorOLS = sm.OLS(endog=y, exog=X_opt).fit()
+regressorOLS.summary()
+
+
+# Automatic version
+import statsmodels.formula.api as sm
+def backwardElimination(x, sl):
+    numVars = len(x[0])
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = np.delete(x, j, 1)
+    regressor_OLS.summary()
+    return x
+ 
+SL = 0.05
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+X_Modeled = backwardElimination(X_opt, SL)
+
+
+
+ 
