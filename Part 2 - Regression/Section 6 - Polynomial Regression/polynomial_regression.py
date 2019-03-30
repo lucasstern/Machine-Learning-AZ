ef3f7075aac8b86fb07d18b19c5b86935019115e
luguistern <lu.guistern@gmail.com>
2019-03-29 23:57:32 -0300

    Start polinomial linear regsression

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2:3].values
+y = dataset.iloc[:, 2].values
 
 # Splitting the dataset into the Training set and Test set
 '''from sklearn.cross_validation import train_test_split
@@ -21,3 +21,48 @@ X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Linear Regression to the datasel
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+X_poly = poly_reg.fit_transform(X)
+
+lin_reg2 = LinearRegression()
+lin_reg2.fit()
+-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
